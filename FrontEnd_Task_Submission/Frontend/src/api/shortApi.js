d259ce6909317b7b8da2b1f982e9cb3049542ba4
Chandu5342 <karrichandu309@gmail.com>
2025-09-04 12:41:39 +0530

    frontend api end to end setup src/api/shortApi.js file

--- FrontEnd_Task_Submission/Frontend/src/api/shortApi.js
+++ FrontEnd_Task_Submission/Frontend/src/api/shortApi.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
+
 const API = axios.create({
   baseURL: "http://localhost:5000",
 });
 
 export const createShortUrl = async (url, validity) => {
   const res = await API.post("/shorturls", { url, validity });
+  console.log(res);
   return res.data;
 };
 
